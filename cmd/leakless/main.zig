eefe424f4fb0b47f59b858fc2f897d70ad14f68e
Yad Smood <y.s.inside@gmail.com>
2022-07-05 19:35:55 +0800

    up

--- cmd/leakless/main.zig
+++ cmd/leakless/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 const stdout = std.io.getStdOut().writer();
 var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
 const allocator = arena.allocator();
@@ -13,7 +14,7 @@ pub fn main() anyerror!void {
 
     var p = try run(args[2..]);
 
-    _ = try std.Thread.spawn(.{}, guard, .{args[1], p});
+    _ = try std.Thread.spawn(.{}, guard, .{ args[1], p.pid });
 
     std.os.exit(switch (try p.wait()) {
         std.ChildProcess.Term.Exited => |v| v,
@@ -30,12 +31,22 @@ fn run(args: []const []const u8) !*std.ChildProcess {
     return p;
 }
 
-fn guard(port: []u8, p: *std.ChildProcess) !void {
-    defer _ = p.kill() catch std.ChildProcess.Term.Unknown;
+fn guard(port: []u8, pid: std.os.system.pid_t) !void {
+    defer kill(pid);
 
     const addr = try std.net.Address.parseIp("127.0.0.1", try std.fmt.parseInt(u16, port, 10));
     const sock = try std.net.tcpConnectToAddress(addr);
 
     var buf: [1]u8 = undefined;
     _ = sock.reader().read(&buf) catch 0;
+}
+
+fn kill(pid: std.os.system.pid_t) void {
+    if (builtin.os.tag == .windows) {
+        const pid_str = std.fmt.comptimePrint("{}", .{pid});
+        var p = try std.ChildProcess.init([]u8{"taskkill", "/t", "/f", "/pid", pid_str}, allocator);
+        _ = p.spawnAndWait() catch std.ChildProcess.Term.Unknown;
+    } else {
+        _ = std.os.kill(-pid, std.os.SIG.KILL) catch std.ChildProcess.Term.Unknown;
+    }
 }
